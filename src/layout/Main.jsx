b45ce434bccf7172321dad44a585f91c6ed5e705
Yan Yu <bx6xbChosen1@gmail.com>
2023-12-17 20:22:16 +0600

    changed protocol to https

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,21 +12,29 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=Matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=Matrix`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err)
+        this.setState({ loading: false })
+      })
   }
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true })
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
       .then((data) => {
         this.setState({ movies: data.Search, loading: false })
+      })
+      .catch((err) => {
+        console.error(err)
+        this.setState({ loading: false })
       })
   }
 
